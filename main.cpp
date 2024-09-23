906679cea3a9381dd792d2a0c5d2d4b327fc2b43
elze-vo <evoveriunaite@gmail.com>
2024-09-23 20:46:28 +0300

    Update main.cpp
    pridejau daugiau teksto random

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 
 int main() { std::cout << "Hello World!\n"; }
+
+
+
+int main() {
+    Studentas studentas;
+    int namuDarbaiKiekis;
+    int laikinasRezultatas;
+
+    // Įvedame studento vardą ir pavardę
+    cout << "Iveskite stu
